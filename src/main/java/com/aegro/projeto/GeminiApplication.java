0dcadf3c428f70056acb77b7c1a2dd6c3faff10a
André Vitor Gabriel <andrevitor.gabriel@gmail.com>
2025-05-07 10:12:35 -0300

    removido codigo antigo

--- src/main/java/com/aegro/projeto/GeminiApplication.java
+++ src/main/java/com/aegro/projeto/GeminiApplication.java
@@ -1,36 +1,3 @@
-// código anterior comentado
-/*
-package com.aegro.projeto;
-
-import com.aegro.projeto.model.ReciboCargaInfo;
-import com.aegro.projeto.service.ExcelExporter;
-import com.aegro.projeto.service.GeminiImageAnalyzer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class GeminiApplication implements CommandLineRunner {
-
-    @Autowired
-    private GeminiImageAnalyzer geminiImageAnalyzer;
-
-    @Autowired
-    private ExcelExporter excelExporter;
-
-    public static void main(String[] args) {
-        SpringApplication.run(GeminiApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        ReciboCargaInfo info = geminiImageAnalyzer.analisarImagem();
-        excelExporter.exportToExcel(info);
-        System.out.println("Extração e exportação concluídas.");
-    }
-}
-*/
 package com.aegro.projeto;
 
 import org.springframework.boot.SpringApplication;
